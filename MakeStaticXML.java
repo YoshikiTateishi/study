2b838f774a956f743738cb1e1a9b0f307eba0a4a
YoshikiTateishi <44398002+YoshikiTateishi@users.noreply.github.com>
2018-12-06 19:38:29 +0900

    静的テストXML作成プログラム

---
+++ MakeStaticXML.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xml;
+
+import java.io.File;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author yoshi
+ */
+public class MakeStaticXML extends Application {
+    
+    Stage primaryStage;
+    Document document;
+    int cnt = 2;
+            
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        //ここに採点するクラス名を入力
+        TicketCalculator app = new TicketCalculator();
+        app.start(primaryStage);
+        getNodeList();
+        // XMLファイルの作成
+        File file = new File("StaticTester.xml");
+        write(file, document);
+        primaryStage.close();
+        System.out.println("XMLを作成しました。");
+    }
+    
+    void getNodeList() throws Exception {
+        // Documentインスタンスの生成
+        DocumentBuilder documentBuilder;
+        documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        document = documentBuilder.newDocument();
+        
+        //シーン
+        Scene scene1 = primaryStage.getScene();      // Scene
+        Element scene = document.createElement("Scene");
+        scene.setAttribute("ID", "0");
+        document.appendChild(scene);
+        
+        //レイアウトペイン
+        Parent root = scene1.getRoot();
+        String nodeType = root.getClass().getSimpleName();
+        Element Box = document.createElement(nodeType);
+        Box.setAttribute("ID", "1");
+        PaneHantei((Node) root, Box);    //POSデータ判定、書き込み
+        
+        //コンポーネント
+        getCom(Box, root);
+        scene.appendChild(Box);
+    }
+    
+    //コンポーネント読み込みメソッド
+    void getCom(Element cp, Parent p) {
+        ObservableList<Node> children = p.getChildrenUnmodifiable();
+        if (children != null) {
+            for (int i=0; i<children.size(); i++) {
+                String com = children.get(i).getClass().getSimpleName();      // コンポーネント名
+                Element Comp = document.createElement(com);
+                Comp.setAttribute("ID", String.valueOf(cnt));
+                cnt++;
+                addText(Comp, children.get(i));
+                cp.appendChild(Comp);
+                Node node = children.get(i);
+                if (node instanceof Pane) {     //レイアウトペイン判定
+                    PaneHantei((Node)node, Comp);
+                    getCom(Comp, (Parent) node);
+                }
+            } 
+        }
+    }
+    
+    //XML書き込みメソッド
+    boolean write(File file, Document document) {
+        // Transformerインスタンスの生成
+        Transformer transformer;
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformer = transformerFactory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            return false;
+        }
+        
+        // Transformerの設定
+        transformer.setOutputProperty("indent", "yes"); //改行指定
+        transformer.setOutputProperty("encoding", "UTF-8"); // エンコーディング
+        
+        // XMLファイルの作成
+        try {
+            transformer.transform(new DOMSource(document), new StreamResult(file));
+        } catch (TransformerException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    //Posデータ追加メソッド
+    void PaneHantei(Node r, Element el) {
+        if(r instanceof VBox) {
+            VBox pane = (VBox) r;
+            el.setAttribute("Pos", pane.getAlignment().toString());       //Pos配置
+        }
+        else if(r instanceof HBox) {
+            HBox pane = (HBox) r;
+            el.setAttribute("Pos", pane.getAlignment().toString());
+        }
+    }
+    
+    //テキスト追加メソッド
+    void addText(Element el, Node node) {
+        if (node instanceof TextField) {
+            TextField tf = (TextField) node;
+            el.appendChild(document.createTextNode(tf.getText()));
+        }
+        else if(node instanceof ComboBox) {
+            ComboBox cb = (ComboBox) node;
+            el.appendChild(document.createTextNode(cb.getItems().toString()));
+        }
+        else {
+            String[] st = node.toString().split("'");
+            if (st.length == 2)
+                el.appendChild(document.createTextNode((st[1]))); 
+        }
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
